4d4c929cf569463aad5e78d0151c923dba989c0d
umerk27235 <umerk@circadia.health>
2025-06-28 02:48:38 +0500

    MOD - config

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  plugins: [react()],
   base: "/Video-Calling-App/",
-  server: {
-    host: true,
-  },
+  plugins: [react()],
 });
