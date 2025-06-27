1d9867a275bef8066aaf18a8cd8e43944015ce14
umerk27235 <umerk@circadia.health>
2025-06-28 02:53:36 +0500

    MOD - hashrouter

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,15 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom"; // 👈 change here
 import "./index.css";
 import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter>
+    <HashRouter>
+      {" "}
+      {/* 👈 use HashRouter */}
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>
 );
